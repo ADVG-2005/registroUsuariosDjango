f1ba7e6fdd3dcd284d254ffd4836d33ef244ec01
advg-2005 <advguwu.09.02.20005@gmail.com>
2024-08-16 10:16:07 -0500

    añadiendo la app

--- users/models.py
+++ users/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    pepe = "as"
+    ESTADO_CHOICES = [
+        ('activo','Activo'),
+        ('inactivo','Inactivo')
+    ]
+    name=models.CharField(max_length=100)
+    cedula=models.CharField(max_length=100)
+    telefono=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100)
+    dateCreated=models.DateField(auto_now=True)
+    estado=models.CharField(max_length=100,choices=ESTADO_CHOICES,default='activo')
